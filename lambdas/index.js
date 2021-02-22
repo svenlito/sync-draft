34f3559ef9d7c9af3f3f2c72e70fcd5ceb220b1a
Nivin <nivincp@gmail.com>
2021-02-22 18:30:53 +0700

    initial commit

---
+++ lambdas/index.js
@@ -0,0 +1,9 @@
+exports.handler = async function (event, context) {
+  console.log(event);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "hello world",
+    }),
+  };
+};
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "lambda_function_zip" {
+  default = "./lambdas/hello.zip"
+}
+
+variable "lambda_function_name" {
+  default = "hello"
+}
